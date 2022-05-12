df0b6fa5df6d93f8285cc124effe1d776e1824d3
alexzhang1030 <1642114555@qq.com>
2022-05-12 23:18:58 +0800

    feat: add Todo layout

--- src/components/ToDo.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import Item from './ToDoItem.svelte'
-</script>
-
-<div class="w-full flex justify-center mt-4">
-  <Item />
-</div>
---
+++ src/views/Index/ToDo.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+  import type { Issue } from '../../api/model'
+</script>
+
+<script lang="ts">
+  import Item from './ToDoItem.svelte'
+  import { ISSUES_URL, http } from '../../api'
+  import { issues } from './data'
+
+  let issuesData: Issue[] = issues as unknown as Issue[]
+
+  console.log({ issuesData })
+
+  async function fetch() {
+    const data = await http<Issue[]>(`${ISSUES_URL}?state=all`)
+    console.log({ data })
+  }
+  // fetch()
+</script>
+
+<div class="w-full flex justify-center mt-4 flex-wrap gap-[20px]">
+  {#each issuesData as issue}
+    <Item {issue} />
+  {/each}
+</div>
