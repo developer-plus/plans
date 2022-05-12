fc085a077bd8af491d6790e6333c83d7c615cb5b
alexzhang1030 <1642114555@qq.com>
2022-05-12 23:21:09 +0800

    chore: remove redandacy code

--- src/views/Index/ToDo.svelte
+++ src/views/Index/ToDo.svelte
@@ -5,17 +5,16 @@
 <script lang="ts">
   import Item from './ToDoItem.svelte'
   import { ISSUES_URL, http } from '../../api'
-  import { issues } from './data'
+  // fake data only for dev
+  // import { issues } from './data'
 
-  let issuesData: Issue[] = issues as unknown as Issue[]
-
-  console.log({ issuesData })
+  let issuesData: Issue[] = []
 
   async function fetch() {
     const data = await http<Issue[]>(`${ISSUES_URL}?state=all`)
-    console.log({ data })
+    issuesData = data
   }
-  // fetch()
+  fetch()
 </script>
 
 <div class="w-full flex justify-center mt-4 flex-wrap gap-[20px]">
